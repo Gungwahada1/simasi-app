bb046a205e838cc55a86cdd223d68a478f657511
unknown <kayikasandya@gmail.com>
2024-09-19 20:34:57 +0800

    upated controller

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -14,91 +14,98 @@ class RoleController extends Controller
 {
     function __construct()
     {
-        $this->middleware('permission:role-list|role-create|role-edit|role-delete', ['only' => ['index','store']]);
-        $this->middleware('permission:role-create', ['only' => ['create','store']]);
-        $this->middleware('permission:role-edit', ['only' => ['edit','update']]);
-        $this->middleware('permission:role-delete', ['only' => ['destroy']]);
+        // $this->middleware('permission:role-list|role-create|role-edit|role-delete', ['only' => ['index','store']]);
+        // $this->middleware('permission:role-create', ['only' => ['create','store']]);
+        // $this->middleware('permission:role-edit', ['only' => ['edit','update']]);
+        // $this->middleware('permission:role-delete', ['only' => ['destroy']]);
     }
 
     public function index(Request $request): View
     {
-        $roles = Role::orderBy('id','DESC')->paginate(5);
-        return view('roles.index',compact('roles'))
-            ->with('i', ($request->input('page', 1) - 1) * 5);
+        // $roles = Role::orderBy('id','DESC')->paginate(5);
+        // return view('roles.index',compact('roles'))
+        //     ->with('i', ($request->input('page', 1) - 1) * 5);
+        return view('dashboard');
     }
 
     public function create(): View
     {
         $permission = Permission::get();
         return view('roles.create',compact('permission'));
+        return view('dashboard');
     }
 
-    public function store(Request $request): RedirectResponse
+    public function store(Request $request)/*: RedirectResponse*/
     {
-        $this->validate($request, [
-            'name' => 'required|unique:roles,name',
-            'permission' => 'required',
-        ]);
+        // $this->validate($request, [
+        //     'name' => 'required|unique:roles,name',
+        //     'permission' => 'required',
+        // ]);
 
-        $permissionsID = array_map(
-            function($value) { return (int)$value; },
-            $request->input('permission')
-        );
+        // $permissionsID = array_map(
+        //     function($value) { return (int)$value; },
+        //     $request->input('permission')
+        // );
 
-        $role = Role::create(['name' => $request->input('name')]);
-        $role->syncPermissions($permissionsID);
+        // $role = Role::create(['name' => $request->input('name')]);
+        // $role->syncPermissions($permissionsID);
 
-        return redirect()->route('roles.index')
-            ->with('success','Role created successfully');
+        // return redirect()->route('roles.index')
+        //     ->with('success','Role created successfully');
+            return view('dashboard');
     }
 
     public function show($id): View
     {
-        $role = Role::find($id);
-        $rolePermissions = Permission::join("role_has_permissions","role_has_permissions.permission_id","=","permissions.id")
-            ->where("role_has_permissions.role_id",$id)
-            ->get();
+        // $role = Role::find($id);
+        // $rolePermissions = Permission::join("role_has_permissions","role_has_permissions.permission_id","=","permissions.id")
+        //     ->where("role_has_permissions.role_id",$id)
+        //     ->get();
 
-        return view('roles.show',compact('role','rolePermissions'));
+        // return view('roles.show',compact('role','rolePermissions'));
+        return view('dashboard');
     }
 
     public function edit($id): View
     {
-        $role = Role::find($id);
-        $permission = Permission::get();
-        $rolePermissions = DB::table("role_has_permissions")->where("role_has_permissions.role_id",$id)
-            ->pluck('role_has_permissions.permission_id','role_has_permissions.permission_id')
-            ->all();
+        // $role = Role::find($id);
+        // $permission = Permission::get();
+        // $rolePermissions = DB::table("role_has_permissions")->where("role_has_permissions.role_id",$id)
+        //     ->pluck('role_has_permissions.permission_id','role_has_permissions.permission_id')
+        //     ->all();
 
-        return view('roles.edit',compact('role','permission','rolePermissions'));
+        // return view('roles.edit',compact('role','permission','rolePermissions'));
+        return view('dashboard');
     }
 
-    public function update(Request $request, $id): RedirectResponse
+    public function update(Request $request, $id)/*: RedirectResponse*/
     {
-        $this->validate($request, [
-            'name' => 'required',
-            'permission' => 'required',
-        ]);
+        // $this->validate($request, [
+        //     'name' => 'required',
+        //     'permission' => 'required',
+        // ]);
 
-        $role = Role::find($id);
-        $role->name = $request->input('name');
-        $role->save();
+        // $role = Role::find($id);
+        // $role->name = $request->input('name');
+        // $role->save();
 
-        $permissionsID = array_map(
-            function($value) { return (int)$value; },
-            $request->input('permission')
-        );
+        // $permissionsID = array_map(
+        //     function($value) { return (int)$value; },
+        //     $request->input('permission')
+        // );
 
-        $role->syncPermissions($permissionsID);
+        // $role->syncPermissions($permissionsID);
 
-        return redirect()->route('roles.index')
-            ->with('success','Role updated successfully');
+        // return redirect()->route('roles.index')
+        //     ->with('success','Role updated successfully');
+            return view('dashboard');
     }
 
-    public function destroy($id): RedirectResponse
+    public function destroy($id)/*: RedirectResponse*/
     {
-        DB::table("roles")->where('id',$id)->delete();
-        return redirect()->route('roles.index')
-            ->with('success','Role deleted successfully');
+        // DB::table("roles")->where('id',$id)->delete();
+        // return redirect()->route('roles.index')
+        //     ->with('success','Role deleted successfully');
+            return view('dashboard');
     }
 }
