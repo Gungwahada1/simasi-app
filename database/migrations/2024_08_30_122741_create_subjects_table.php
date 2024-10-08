dd2b3d20503e1d7996d3638d2624369eb7617bff
PutuJona <putujona88@gmail.com>
2024-10-08 21:24:19 +0800

    CRUD Subject

--- database/migrations/2024_08_30_122741_create_subjects_table.php
+++ database/migrations/2024_08_30_122741_create_subjects_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -12,9 +13,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('subjects', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->uuid('id')->primary()->default(DB::raw('(UUID())'));
             $table->string('subject_name');
-            $table->text('subject-description')->nullable();
+            $table->text('subject_description')->nullable();
             $table->timestamp('created_at')->nullable();
             $table->timestamp('created_by')->nullable();
             $table->timestamp('updated_at')->nullable();
